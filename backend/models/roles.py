fb823092c49cb5a69c5b2a1be8d232a4c1182141
Arash84Prhsn <poorhasaniarash@gmail.com>
2026-05-31 14:39:39 +0330

    Updated docs to include the new mock data

--- backend/models/roles.py
+++ backend/models/roles.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, DateTime, CheckConstraint, Boolean, Text
 from sqlalchemy.orm import relationship
 from backend.models import DeclarativeBase
+from datetime import datetime
 
 
 class Role(DeclarativeBase):
@@ -9,3 +10,4 @@ class Role(DeclarativeBase):
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String, unique=True, nullable=False)
     description = Column(Text)
+    created_at = Column(DateTime, default=datetime.now)
