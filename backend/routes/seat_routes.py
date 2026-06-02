2b6cf81ab1f042cbb441d2988a5548f6fa81f149
Arash84Prhsn <poorhasaniarash@gmail.com>
2026-06-02 16:54:47 +0330

    Added the new seats endpoint

--- backend/routes/seat_routes.py
+++ backend/routes/seat_routes.py
@@ -5,7 +5,6 @@ from backend.models.enums import SEAT_COUNTS
 
 seat_bp = Blueprint('seats', __name__, url_prefix='/api/seats')
 
-#TODO ADD THIS TO OPENAPI DOCS
 @seat_bp.route("/get_seat_types_and_counts", methods=['GET'])
 def get_seat_types_and_counts():
     d = {}
