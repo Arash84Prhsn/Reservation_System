76fa1b616741f12db35cb6aa1df17e44b5c2496c
Arash84Prhsn <poorhasaniarash@gmail.com>
2026-06-12 15:32:05 +0330

    Automatic Logout and redirect to Login is done

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ CORS(app,
 app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(hours=12)
 app.config["SESSION_PERMANENT"] = True
 
-IDLE_TIMEOUT = timedelta(seconds=10)
+IDLE_TIMEOUT = timedelta(minutes=30)
 
 @app.before_request
 def enforce_idle_timeout():
@@ -56,7 +56,7 @@ def enforce_idle_timeout():
     # Check inactivity
     if now - last_time > IDLE_TIMEOUT:
         session.clear()
-        return redirect("http://localhost:3000/login")
+        return redirect("http://localhost:3000/signin")
 
     # update activity timestamp
     session["last_activity"] = now.isoformat()
